183fdb66d846a7ffeaf6abd91dbe93303be1925b
Agbeluyi Joshua Temitayo <joshuaagbeluyi20@gmail.com>
2025-05-28 11:29:34 +0100

    fixed redirect url

--- orders/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from .models import Order
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'full_name', 'email', 'amount_paid', 'shipped', 'date_ordered']
-    list_filter = ['shipped', 'date_ordered']
-    search_fields = ['full_name', 'email', 'tx_ref']
